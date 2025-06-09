4f17f17de9c8a8d5d5d47acb4908b709c1e1fb9f
Sebastian Cancinos <sebastian.cancinos@woovapp.com>
2025-06-09 12:39:22 +0200

    Build deeplink url using QueryItems for clarity

--- LocationsList/LocationsList/LocationsListRouter.swift
+++ LocationsList/LocationsList/LocationsListRouter.swift
@@ -5,10 +5,14 @@ class LocationsListRouter: LocationsListRoutingLogic {
     func routeToWikipedia(for location: Location) {
         // TODO: Implement Wikipedia navigation
 
-        guard let url = URL(string: "https://en.wikipedia.org/wiki/location?lat=\(location.latitude)&lng=\(location.longitude)") else {
+        guard let baseURL = URL(string: "https://en.wikipedia.org/wiki/location") else {
             return
         }
 
+        let latitude = URLQueryItem(name: "lat", value: String(location.latitude))
+        let longitude = URLQueryItem(name: "lng", value: String(location.longitude))
+
+        let url = baseURL.appending(queryItems: [latitude, longitude])
         UIApplication.shared.open(url)
     }
 } 
